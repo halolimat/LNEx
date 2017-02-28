c00809e0e922eb57902424ba0e35f60b77034be7
Hussein Al-Olimat <hussein@knoesis.org>
2017-02-28 10:56:48 -0500

    Create sample_tweets.py

---
+++ Data/sample_tweets.py
@@ -0,0 +1,5 @@
+#ChennaiFloods A pregnant lady rescued from Mudichur Near tambaram around 1130 this morning by Indian Navy https://t.co/I5ZYe4
+#ChennaiFloods #'Saidapet', food available for 700-people;no.4,pilliyarkoil street,Jones road subway.call Dinesh Thomas @ 04
+#ChennaiFloods  All #trains to & from Chennai Central & Chennai Egmore #cancelled till 12 hrs on Dec5, says SouthernRailway
+Water Stagnation in New Avadi road and Water tank road, #Kilpauk . Power available. #ChennaiFloods #ChennaiRainsHelp @
+Unable to reach a frnds family residing at Old no. 9/New no. 34. Strt no. 4, Postal colony, west mambalam, Chennai. Pl
