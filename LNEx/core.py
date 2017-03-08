602ad0e772fdba576649dcf25e426fdd0900f10e
Hussein <hussein@knoesis.org>
2017-03-08 13:46:18 -0500

    core

--- LNEx/core.py
+++ LNEx/core.py
@@ -1032,6 +1032,41 @@ class init_env:
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+import sys
+from numbers import Number
+from collections import Set, Mapping, deque
+
+try: # Python 2
+    zero_depth_bases = (basestring, Number, xrange, bytearray)
+    iteritems = 'iteritems'
+except NameError: # Python 3
+    zero_depth_bases = (str, bytes, Number, range, bytearray)
+    iteritems = 'items'
+
+def getsize(obj_0):
+    """Recursively iterate to sum size of object & members."""
+    def inner(obj, _seen_ids = set()):
+        obj_id = id(obj)
+        if obj_id in _seen_ids:
+            return 0
+        _seen_ids.add(obj_id)
+        size = sys.getsizeof(obj)
+        if isinstance(obj, zero_depth_bases):
+            pass # bypass remaining control flow and return
+        elif isinstance(obj, (tuple, list, Set, deque)):
+            size += sum(inner(i) for i in obj)
+        elif isinstance(obj, Mapping) or hasattr(obj, iteritems):
+            size += sum(inner(k) + inner(v) for k, v in getattr(obj, iteritems)())
+        # Check for custom object instances - may subclass above too
+        if hasattr(obj, '__dict__'):
+            size += inner(vars(obj))
+        if hasattr(obj, '__slots__'): # can have __slots__ with __dict__
+            size += sum(inner(getattr(obj, s)) for s in obj.__slots__ if hasattr(obj, s))
+        return size
+    return inner(obj_0)
+
+
+
 def start_using_files():
 
     # chennai flood bounding box
@@ -1065,6 +1100,8 @@ def start_using_files():
     print tabulate(rows, headers=header)
     print "-" * 90
 
+    print getsize(env)
+
 
 def start_using_elastic_index():
 
@@ -1097,6 +1134,6 @@ def start_using_elastic_index():
 
 if __name__ == "__main__":
 
-    #start_using_files()
+    start_using_files()
 
-    start_using_elastic_index()
+    #start_using_elastic_index()
